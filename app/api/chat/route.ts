9dddc409831bb0f848e7b0dd6b37c08b84348bc3
George Campbell <campbell.goe@gmail.com>
2024-02-13 09:00:30 +0000

    update chat api to use mistral

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,18 +3,20 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  baseURL: 'http://localhost:11434/v1',
+  apiKey: 'ollama',
 });
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
+  try {
   const { messages } = await req.json();
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'mistral',
     stream: true,
     messages,
   });
@@ -23,4 +25,7 @@ export async function POST(req: Request) {
   const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
+} catch(err){
+  return Response.json({ error: err })
+}
 }
